ca1b160270612c687ef35e800ce61726aa5a0a88
vinay-jnv <109013626+vinay-jnv@users.noreply.github.com>
2023-04-09 22:49:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer","C++ Coder","Student", "Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    baxkDelay: 1000,
+    loop: true
+}) 
